5a879b38d591379e109d2a09a2d71e593fbbf546
Damian Wilkołek <dwilkolek@protonmail.com>
2023-04-13 01:13:03 +0200

    speed up loading

--- src-tauri/src/aws.rs
+++ src-tauri/src/aws.rs
@@ -1,15 +1,15 @@
 use std::fmt;
 
+use crate::shared::BError;
 use aws_sdk_ec2 as ec2;
 use aws_sdk_ecs as ecs;
 use aws_sdk_rds as rds;
 use aws_sdk_secretsmanager as secretsmanager;
 use aws_sdk_ssm as ssm;
+use chrono::prelude::*;
 use ec2::types::Filter;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
-use chrono::prelude::*;
-use crate::shared::BError;
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
 pub enum Env {
@@ -220,7 +220,6 @@ pub async fn service_details(ecs: &ecs::Client, service_arn: &str) -> ServiceDet
     let service = service.services().unwrap();
     let service = &service[0];
     let task_def_arn = service.task_definition().unwrap();
-
     let task_def = ecs
         .describe_task_definition()
         .task_definition(task_def_arn)
