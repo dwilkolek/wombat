6ada63cac765476551e5213f0c67dbdb84a7fe55
Damian Wilkołek <damian.wilkolek@technipfmc.com>
2023-05-08 18:42:15 +0200

    fixes

--- src/routes/logged/home/service-cell.svelte
+++ src/routes/logged/home/service-cell.svelte
@@ -21,9 +21,11 @@
 {#if service}
 	<div class="flex flex-row gap-2 items-start pr-4">
 		{#if $taskStore.find((t) => t.arn == service?.arn)}
-			<button on:click={async () => {
-				await execute('stop_job', { arn: service?.arn });
-			}}>
+			<button
+				on:click={async () => {
+					await execute('stop_job', { arn: service?.arn });
+				}}
+			>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					viewBox="0 0 20 20"
@@ -77,20 +79,10 @@
 							: ''
 					}`}>{service.version ?? '??'}</span
 				>
-				<!-- <svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-					class="w-5 h-5"
-				>
-					<path
-						d="M4.25 2A2.25 2.25 0 002 4.25v2a.75.75 0 001.5 0v-2a.75.75 0 01.75-.75h2a.75.75 0 000-1.5h-2zM13.75 2a.75.75 0 000 1.5h2a.75.75 0 01.75.75v2a.75.75 0 001.5 0v-2A2.25 2.25 0 0015.75 2h-2zM3.5 13.75a.75.75 0 00-1.5 0v2A2.25 2.25 0 004.25 18h2a.75.75 0 000-1.5h-2a.75.75 0 01-.75-.75v-2zM18 13.75a.75.75 0 00-1.5 0v2a.75.75 0 01-.75.75h-2a.75.75 0 000 1.5h2A2.25 2.25 0 0018 15.75v-2zM7 10a3 3 0 116 0 3 3 0 01-6 0z"
-					/>
-				</svg> -->
 			</button>
 		{/if}
 	</div>
 {/if}
 {#if !service}
-	<div>N/A</div>
+	<div />
 {/if}
