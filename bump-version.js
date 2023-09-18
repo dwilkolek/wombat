ffa1eb617090db43b055b3bb5f17daa92e391245
Damian Wilkołek <damian.wilkolek@technipfmc.com>
2023-09-18 09:43:44 +0200

    Release v1.0.8

--- bump-version.js
+++ bump-version.js
@@ -23,6 +23,8 @@ fs.writeFileSync(
 	cargoToml.substring(0, start) + `version = "${newVersion}"` + cargoToml.substring(end)
 );
 
+cp.exec('npm install');
+cp.exec('cd src-tauri && cargo generate-lockfile && cd ..');
 cp.exec(`git commit -a -m"Release v${newVersion}"`);
 cp.exec(`git tag v${newVersion}`);
 cp.exec(`git push origin v${newVersion}`);
