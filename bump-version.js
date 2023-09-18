bccae4d4919cd8cc6d1a9d6eb067fa6f7c13ef67
Damian Wilkołek <damian.wilkolek@technipfmc.com>
2023-09-18 09:29:55 +0200

    Release v1.0.7

--- bump-version.js
+++ bump-version.js
@@ -1,4 +1,5 @@
 import * as fs from 'node:fs';
+import * as cp from 'child_process';
 
 const newVersion = `${process.argv[2]}`;
 console.log(`New version ${newVersion}`);
@@ -15,11 +16,12 @@ fs.writeFileSync(tauriConfPath, JSON.stringify(tauriConfJson, null, 2));
 
 const cargoTomlPath = './src-tauri/Cargo.toml';
 const cargoToml = fs.readFileSync(cargoTomlPath).toString('utf8');
-const start = cargoToml.search(/version = \"([0-9\.]+)\"/gm);
+const start = cargoToml.search(/version = "([0-9.]+)"/gm);
 const end = cargoToml.indexOf('\n', start);
 fs.writeFileSync(
 	cargoTomlPath,
 	cargoToml.substring(0, start) + `version = "${newVersion}"` + cargoToml.substring(end)
 );
-
+cp.exec(`git commit -a -m"Release v${newVersion}"`);
+cp.exec(`git push origin v${newVersion}`);
 console.log(`Done`);
