dbb8b960dcdc4ca49a21c631bbecd44e98f5a4a1
Damian Wilkołek <dwilkolek@protonmail.com>
2023-10-08 16:59:09 +0200

    Release v1.3.0.alpha-1

--- bump-version.js
+++ bump-version.js
@@ -2,25 +2,27 @@ import * as fs from 'node:fs';
 import * as cp from 'child_process';
 
 const newVersion = `${process.argv[2]}`;
-console.log(`New version ${newVersion}`);
+const safeNewVersion = newVersion.match(/([0-9]+.[0-9]+.[0-9]+)/)[0]
+
+console.log(`New version ${newVersion}(${safeNewVersion})`);
 
 const packageJsonPath = './package.json';
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString('utf8'));
-packageJson['version'] = newVersion;
+packageJson['version'] = safeNewVersion;
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 4));
 
 const tauriConfPath = './src-tauri/tauri.conf.json';
 const tauriConfJson = JSON.parse(fs.readFileSync(tauriConfPath).toString('utf8'));
-tauriConfJson['package']['version'] = newVersion;
+tauriConfJson['package']['version'] = safeNewVersion;
 fs.writeFileSync(tauriConfPath, JSON.stringify(tauriConfJson, null, 2));
 
 const cargoTomlPath = './src-tauri/Cargo.toml';
 const cargoToml = fs.readFileSync(cargoTomlPath).toString('utf8');
-const start = cargoToml.search(/version = "([0-9.]+)"/gm);
+const start = cargoToml.search(/version = "([0-9a-z-.]+)"/gm);
 const end = cargoToml.indexOf('\n', start);
 fs.writeFileSync(
 	cargoTomlPath,
-	cargoToml.substring(0, start) + `version = "${newVersion}"` + cargoToml.substring(end)
+	cargoToml.substring(0, start) + `version = "${safeNewVersion}"` + cargoToml.substring(end)
 );
 
 cp.execSync('npm install');
