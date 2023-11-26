529a568c42bfdf0d9904abf7dd956d8158f11df3
Damian Wilkołek <dwilkolek@users.noreply.github.com>
2023-11-26 15:14:30 +0100

    feat: dump logs to file (#31)

--- src/routes/logged/logs/+page.svelte
+++ src/routes/logged/logs/+page.svelte
@@ -141,6 +141,32 @@
 
 					Search!</button
 				>
+				<button
+					class="btn btn-active btn-primary"
+					disabled={!$selectedService || $selectedService?.env !== $activeCluser.env}
+					on:click={() => {
+						if ($selectedService?.name && $activeCluser?.env) {
+							logStore.dumpLogs($selectedService.name, $activeCluser.env);
+						}
+					}}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+						/>
+					</svg>
+
+					Dump logs</button
+				>
 			{/if}
 			{#if $storeState.isLookingForLogs}
 				<button
