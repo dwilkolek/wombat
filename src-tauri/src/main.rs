e12f8739a6e89a716445df2ef227faebb9add9f5
Damian Wilkołek <dwilkolek@users.noreply.github.com>
2024-02-18 22:17:09 +0100

    Fix kill process (#44)
    fix: prevent killing wombat when killing sessionmanager proxy
    chore: remove port-killer
    chore: refactor proxy into separate module

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,6 @@ use aws::{Cluster, DbInstance, DbSecret, EcsService, LogEntry, ServiceDetails};
 use aws_config::BehaviorVersion;
 use axiom_rs::Client;
 use log::{error, info, warn, LevelFilter};
-use port_killer::kill;
 use regex::Regex;
 use serde_json::json;
 use shared::{arn_resource_type, arn_to_name, ecs_arn_to_name, rds_arn_to_name, ResourceType};
@@ -13,7 +12,6 @@ use shared_child::SharedChild;
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader, BufWriter, Write};
-use std::process::Stdio;
 use std::sync::Arc;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use std::{collections::HashMap, process::Command};
@@ -21,22 +19,15 @@ use std::{env, fs};
 use tauri::Window;
 use tauri_plugin_log::LogTarget;
 use tokio::sync::Mutex;
-use tokio::time::sleep;
 use tracing_unwrap::{OptionExt, ResultExt};
 use urlencoding::encode;
 use user::UserConfig;
 use wait_timeout::ChildExt;
-use warp::http::HeaderName;
-use warp::http::HeaderValue;
-use warp::hyper::body::Bytes;
-use warp::hyper::Method;
-use warp::Filter as WarpFilter;
-use warp_reverse_proxy::{extract_request_data_filter, proxy_to_and_forward_response, Headers};
+
 mod aws;
 mod shared;
 mod user;
-use filepath::FilePath;
-use tempdir::TempDir;
+mod proxy;
 
 #[derive(Clone, serde::Serialize)]
 struct ProxyEventMessage {
@@ -520,11 +511,6 @@ async fn stop_job(
                 Ok(output) => info!("Attempted to kill session in SSM: {:?}", output),
                 Err(e) => warn!("Failed to kill session in SSM {}", e),
             };
-        } else {
-            info!(
-                "SessionId {} to kill not found",
-                session_id.unwrap_or_default()
-            )
         }
     } else {
         ingest_log(
@@ -983,7 +969,7 @@ async fn start_db_proxy(
     )
     .await;
 
-    start_aws_ssm_proxy(
+    proxy::start_aws_ssm_proxy(
         db.arn,
         window,
         bastion.instance_id,
@@ -1179,10 +1165,10 @@ async fn start_service_proxy(
     )
     .await;
 
-    let handle = start_proxy_to_aws_proxy(
+    let handle = proxy::start_proxy_to_aws_proxy(
         local_port,
         aws_local_port,
-        ProxyInterceptor {
+        proxy::ProxyInterceptor {
             path_prefix: String::from(""),
             headers: HashMap::from([
                 (String::from("Host"), host.clone()),
@@ -1192,7 +1178,7 @@ async fn start_service_proxy(
     )
     .await;
 
-    start_aws_ssm_proxy(
+    proxy::start_aws_ssm_proxy(
         service.arn,
         window,
         bastion.instance_id,
@@ -1285,188 +1271,6 @@ async fn open_dbeaver(
     return Ok(());
 }
 
-#[derive(Clone)]
-struct ProxyInterceptor {
-    path_prefix: String,
-    headers: HashMap<String, String>,
-}
-
-impl ProxyInterceptor {
-    fn applies(&self, uri: &str) -> bool {
-        return uri.starts_with(&self.path_prefix);
-    }
-    fn modify_headers(&self, headers: &mut Headers) {
-        let h = self.headers.clone();
-        for (name, value) in h.iter() {
-            let header_value = value.parse::<HeaderValue>().unwrap_or_log();
-            let header_name = name.parse::<HeaderName>().unwrap_or_log();
-            headers.insert(header_name, header_value);
-        }
-    }
-}
-
-async fn start_proxy_to_aws_proxy(
-    local_port: u16,
-    aws_local_port: u16,
-    proxy_intercepter: ProxyInterceptor,
-) -> tokio::task::JoinHandle<()> {
-    tokio::task::spawn(async move {
-        let request_filter = extract_request_data_filter();
-
-        let app = warp::any().and(request_filter).and_then(
-            move |uri: warp::path::FullPath,
-                  params: Option<String>,
-                  method: Method,
-                  mut headers: Headers,
-                  body: Bytes| {
-                if proxy_intercepter.applies(uri.as_str()) {
-                    proxy_intercepter.modify_headers(&mut headers);
-                }
-
-                proxy_to_and_forward_response(
-                    format!("http://localhost:{}/", aws_local_port).to_owned(),
-                    "".to_owned(),
-                    uri,
-                    params,
-                    method,
-                    headers,
-                    body,
-                )
-            },
-        );
-        warp::serve(app).run(([0, 0, 0, 0], local_port)).await;
-    })
-}
-
-async fn start_aws_ssm_proxy(
-    arn: String,
-    window: Window,
-    bastion: String,
-    profile: String,
-    target: String,
-    target_port: u16,
-    local_port: u16,
-
-    abort_on_exit: Option<tokio::task::JoinHandle<()>>,
-    access_port: u16,
-    async_task_manager: tauri::State<'_, AsyncTaskManager>,
-) {
-    let mut command = Command::new("aws");
-    command.args([
-        "ssm",
-        "start-session",
-        "--target",
-        &bastion,
-        "--profile",
-        &profile,
-        "--document-name",
-        "AWS-StartPortForwardingSessionToRemoteHost",
-        "--parameters",
-        &format!(
-            "{{\"host\":[\"{}\"], \"portNumber\":[\"{}\"], \"localPortNumber\":[\"{}\"]}}",
-            target, target_port, local_port
-        ),
-    ]);
-
-    kill_pid_on_port(local_port).await;
-
-    let tmp_dir = TempDir::new("wombat").unwrap();
-    let out_log: File =
-        File::create(tmp_dir.path().join(format!("out-{}.log", local_port))).unwrap();
-    let err_log: File =
-        File::create(tmp_dir.path().join(format!("err-{}.log", local_port))).unwrap();
-
-    let out_log_path = format!("{}", out_log.path().unwrap().display());
-    let err_log_path = format!("{}", err_log.path().unwrap().display());
-
-    command.stdout(Stdio::from(out_log));
-    command.stderr(Stdio::from(err_log));
-
-    info!("Out log path: {:?}", out_log_path.clone());
-    info!("Error log path: {:?}", err_log_path.clone());
-    info!("Executnig cmd: {:?} ", command);
-
-    let shared_child = SharedChild::spawn(&mut command).unwrap_or_log();
-
-    let shared_child_arc = Arc::new(shared_child);
-    let child_arc_clone = shared_child_arc.clone();
-
-    async_task_manager
-        .0
-        .lock()
-        .await
-        .proxies_handlers
-        .insert(arn.clone(), shared_child_arc);
-    let started_at = SystemTime::now();
-
-    loop {
-        sleep(Duration::from_millis(100)).await;
-        let contents = fs::read_to_string(out_log_path.clone()).unwrap_or_default();
-
-        info!("Output: {}", contents);
-        if contents.contains("Waiting for connections...") {
-            break;
-        }
-        if SystemTime::now()
-            .duration_since(started_at)
-            .unwrap()
-            .as_secs()
-            > 10
-        {
-            let contents = fs::read_to_string(err_log_path.clone()).unwrap_or_default();
-            error!("Failed to start proxy: {}", contents);
-            window
-                .emit(
-                    "proxy-end",
-                    ProxyEventMessage::new(arn.clone(), "ERROR".into(), access_port),
-                )
-                .unwrap_or_log();
-
-            kill_pid_on_port(local_port).await;
-            return;
-        }
-    }
-    tokio::task::spawn(async move {
-        // {\"host\":[\"$endpoint\"], \"portNumber\":[\"5432\"], \"localPortNumber\":[\"$port\"]}
-        // aws ssm start-session \
-        //  --target "$instance" \
-        //  --profile "$profile" \
-        //  --document-name AWS-StartPortForwardingSessionToRemoteHost \
-        //  --parameters "$parameters"
-
-        window
-            .emit(
-                "proxy-start",
-                ProxyEventMessage::new(arn.clone(), "STARTED".into(), access_port),
-            )
-            .unwrap_or_log();
-        let _ = child_arc_clone.wait();
-
-        if let Some(handle) = abort_on_exit {
-            info!("Killing dependant job");
-            handle.abort();
-        }
-        window
-            .emit(
-                "proxy-end",
-                ProxyEventMessage::new(arn.clone(), "END".into(), access_port),
-            )
-            .unwrap_or_log();
-        kill_pid_on_port(local_port).await;
-    });
-}
-
-async fn kill_pid_on_port(port: u16) {
-    let _ = tokio::task::spawn(async move {
-        info!("Trying to kill process on local port: {}", port);
-        let kill_result = kill(port);
-        match kill_result {
-            Ok(res) => info!("Killed: {}", res),
-            Err(err) => warn!("Killing failed, {}", err),
-        }
-    })
-    .await;
-}
 
 #[tokio::main]
 async fn main() {
