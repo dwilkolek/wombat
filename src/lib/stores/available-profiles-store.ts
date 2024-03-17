db27906314bc78fdfe4c934e873217b9c22f6927
Damian Wilkolek <dwilkolek@protonmail.com>
2024-03-17 01:27:44 +0100

    format

--- src/lib/stores/available-profiles-store.ts
+++ src/lib/stores/available-profiles-store.ts
@@ -1,25 +1,23 @@
-import { invoke } from "@tauri-apps/api";
-import { writable } from "svelte/store";
-
-
+import { invoke } from '@tauri-apps/api';
+import { writable } from 'svelte/store';
 
 const createAvailableProfilesStore = () => {
-    const { subscribe, set, update } = writable<string[]>([]);
-    invoke<string[]>('available_profiles').then((resp) => {
-        set(resp);
-    });
-    const refresh = () => {
-        invoke<string[]>('available_profiles').then((resp) => {
-            set(resp);
-        });
-    }
-    
-    return {
-        refresh,
-        subscribe,
-        set,
-        update,
-    };
-}
+	const { subscribe, set, update } = writable<string[]>([]);
+	invoke<string[]>('available_profiles').then((resp) => {
+		set(resp);
+	});
+	const refresh = () => {
+		invoke<string[]>('available_profiles').then((resp) => {
+			set(resp);
+		});
+	};
+
+	return {
+		refresh,
+		subscribe,
+		set,
+		update
+	};
+};
 
 export const availableProfilesStore = createAvailableProfilesStore();
