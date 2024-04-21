c32c200d613b460dadbee34ffd5c830d67e477c9
Damian Wilkołek <dwilkolek@users.noreply.github.com>
2024-04-22 00:01:57 +0200

    chore: fix html warnings + security issues (#66)

--- src/routes/logged/apps/+page.svelte
+++ src/routes/logged/apps/+page.svelte
@@ -6,7 +6,6 @@
 
 	$: user = $userStore;
 
-	console.log(user);
 	$: selectedClusters = $userStore.preffered_environments;
 
 	$: columnToggleHandler = (env: AwsEnv, e: any) => {
@@ -29,7 +28,6 @@
 	<meta name="description" content="Wombat" />
 </svelte:head>
 <div class="bg-base-100 flex flex-row justify-between px-2 sticky top-[68px] z-50">
-	
 	<form
 		class="flex flex-row gap-1 mb-2"
 		on:submit|preventDefault={async () => {
