c3b5cbc3782a662c80a1aca530eabe69b4add9c8
Damian Wilkolek <dwilkolek@protonmail.com>
2024-04-21 23:32:36 +0200

    chore: fix html warnings + security issues

--- src/lib/componets/app-card-hr.svelte
+++ src/lib/componets/app-card-hr.svelte
@@ -6,28 +6,36 @@
 
 {#if task}
 	{#if task.status == 'STARTING'}
-	<div class={`grow flex gap-1 text-amber-300 relative items-stretch animate-pulse`}>
-		{#if task.proxy_auth_config}
-			<span class="text-xs absolute bottom-0 left-4 w-full">{task.proxy_auth_config?.authType}:&nbsp;{task.proxy_auth_config?.jepsenClientId ?? task.proxy_auth_config?.basicUser ?? '?'}</span>
-			<div class={`bg-lime-600 h-[2px] w-full rounded-full`} />
-		{:else}
-			<div class={`bg-lime-600 h-[2px] w-full rounded-full`} />
-		{/if}
-	</div>
+		<div class={`grow flex gap-1 text-amber-300 relative items-stretch animate-pulse`}>
+			{#if task.proxy_auth_config}
+				<span class="text-xs absolute bottom-0 left-4 w-full"
+					>{task.proxy_auth_config?.authType}:&nbsp;{task.proxy_auth_config?.jepsenClientId ??
+						task.proxy_auth_config?.basicUser ??
+						'?'}</span
+				>
+				<div class={`bg-lime-600 h-[2px] w-full rounded`} />
+			{:else}
+				<div class={`bg-lime-600 h-[2px] w-full rounded`} />
+			{/if}
+		</div>
 	{/if}
 
 	{#if task.status == 'STARTED'}
 		<div class={`grow flex gap-1 text-amber-300 relative items-stretch`}>
 			{#if task.proxy_auth_config}
-				<span class="text-xs absolute bottom-0 left-4 w-full">{task.proxy_auth_config?.authType}:&nbsp;{task.proxy_auth_config?.jepsenClientId ?? task.proxy_auth_config?.basicUser ?? '?'}</span>
-				<div class={`bg-lime-600 h-[2px] w-full rounded-full`} />
+				<span class="text-xs absolute bottom-0 left-4 w-full"
+					>{task.proxy_auth_config?.authType}:&nbsp;{task.proxy_auth_config?.jepsenClientId ??
+						task.proxy_auth_config?.basicUser ??
+						'?'}</span
+				>
+				<div class={`bg-lime-600 h-[2px] w-full rounded`} />
 			{:else}
-				<div class={`bg-lime-600 h-[2px] w-full rounded-full`} />
+				<div class={`bg-lime-600 h-[2px] w-full rounded`} />
 			{/if}
 		</div>
 	{/if}
 {:else}
 	<div class={`grow flex items-center`}>
-		<div class={`bg-base-100 h-[2px] w-full rounded-full`} />
+		<div class={`bg-base-100 h-[2px] w-full rounded`} />
 	</div>
 {/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,114 +46,112 @@
 	<title>LOGIN</title>
 	<meta name="description" content="Wombat" />
 </svelte:head>
-{#await subscribe then _}
-	<div class="hero max-h-screen min-h-screen bg-base-200">
-		<div class="absolute left-4 top-4 p-2">
-			{#await dependenciesPromise then deps}
-				{@const entries = Object.entries(deps).sort((a, b) => a[0].localeCompare(b[0]))}
-				<div class="flex flex-col gap-1">
-					{#each entries as dep}
-						<div class="flex items-center gap-1 text-sm">
-							{#if dep[1].Ok}
-								<div class="bg-lime-500 w-2 h-2 rounded rounded-full" />
-							{:else}
-								<div class="bg-rose-500 w-2 h-2 rounded rounded-full" />
-							{/if}
-							<span>
-								{dep[0]} :
-							</span>
-							<span class="">
-								{dep[1].Ok ?? dep[1].Err}
-							</span>
-						</div>
-					{/each}
-				</div>
-			{/await}
+<div class="hero max-h-screen min-h-screen bg-base-200">
+	<div class="absolute left-4 top-4 p-2">
+		{#await dependenciesPromise then deps}
+			{@const entries = Object.entries(deps).sort((a, b) => a[0].localeCompare(b[0]))}
+			<div class="flex flex-col gap-1">
+				{#each entries as dep}
+					<div class="flex items-center gap-1 text-sm">
+						{#if dep[1].Ok}
+							<div class="bg-lime-500 w-2 h-2 rounded" />
+						{:else}
+							<div class="bg-rose-500 w-2 h-2 rounded" />
+						{/if}
+						<span>
+							{dep[0]} :
+						</span>
+						<span class="">
+							{dep[1].Ok ?? dep[1].Err}
+						</span>
+					</div>
+				{/each}
+			</div>
+		{/await}
+	</div>
+	<div class="hero-content flex-col">
+		<div class="text-center">
+			<h1 class="text-5xl font-medium">Hello!</h1>
+			<p class="py-6">Wombat is friendly app that aims to make your life less miserable 😎</p>
 		</div>
-		<div class="hero-content flex-col">
-			<div class="text-center">
-				<h1 class="text-5xl font-medium">Hello!</h1>
-				<p class="py-6">Wombat is friendly app that aims to make your life less miserable 😎</p>
+		<div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+			<div class="card-body">
+				<form
+					on:submit|preventDefault={async () => {
+						try {
+							loading = true;
+							await login(profile);
+							loading = false;
+							goto(`/logged/apps`, { replaceState: true });
+						} catch (e) {
+							console.error(e);
+							buttonText = 'Start Again';
+							loading = false;
+						}
+					}}
+				>
+					<div class="form-control">
+						<label class="label" for="aws-profile">
+							<span class="label-text">AWS profile</span>
+						</label>
+						<select class="select select-bordered w-full max-w-xs" bind:value={profile}>
+							{#await $ssoProfiles then ssoProfiles}
+								{#each ssoProfiles as ssoProfile}
+									<option value={ssoProfile}>{ssoProfile}</option>
+								{/each}
+							{/await}
+						</select>
+					</div>
+					{#await dependenciesPromise then deps}
+						{#if !Object.entries(deps).some((v) => v[1].Err)}
+							<div class="form-control mt-6">
+								<button class="btn btn-accent" disabled={loading} type="submit">
+									{buttonText}</button
+								>
+							</div>
+						{:else}
+							<div class="text-rose-500">Required dependency is missing</div>
+						{/if}
+					{/await}
+				</form>
 			</div>
-			<div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-				<div class="card-body">
-					<form
-						on:submit|preventDefault={async () => {
-							try {
-								loading = true;
-								await login(profile);
-								loading = false;
-								goto(`/logged/apps`, { replaceState: true });
-							} catch (e) {
-								console.error(e);
-								buttonText = 'Start Again';
-								loading = false;
-							}
-						}}
-					>
-						<div class="form-control">
-							<label class="label" for="aws-profile">
-								<span class="label-text">AWS profile</span>
-							</label>
-							<select class="select select-bordered w-full max-w-xs" bind:value={profile}>
-								{#await $ssoProfiles then ssoProfiles}
-									{#each ssoProfiles as ssoProfile}
-										<option value={ssoProfile}>{ssoProfile}</option>
-									{/each}
-								{/await}
-							</select>
-						</div>
-						{#await dependenciesPromise then deps}
-							{#if !Object.entries(deps).some((v) => v[1].Err)}
-								<div class="form-control mt-6">
-									<button class="btn btn-accent" disabled={loading} type="submit">
-										{buttonText}</button
-									>
-								</div>
-							{:else}
-								<div class="text-rose-500">Required dependency is missing</div>
-							{/if}
-						{/await}
-					</form>
-				</div>
-			</div>
-			<div class="flex flex-col justify-center items-center gap-2 my-2">
-				{#await latest then latest}
-					{#if (latest ?? '0.0.0')
+		</div>
+		<div class="flex flex-col justify-center items-center gap-2 my-2">
+			{#await latest then latest}
+				{#if (latest ?? '0.0.0')
+					.split('.')
+					.map((v, i) => parseInt(v) * Math.pow(1000, 3 - i))
+					.reduce((acc, v) => acc + v, 0) > version
 						.split('.')
 						.map((v, i) => parseInt(v) * Math.pow(1000, 3 - i))
-						.reduce((acc, v) => acc + v, 0) > version
-							.split('.')
-							.map((v, i) => parseInt(v) * Math.pow(1000, 3 - i))
-							.reduce((acc, v) => acc + v, 0)}
-						<a
-							class="underline text-accent"
-							href="https://github.com/dwilkolek/wombat/releases/latest"
-							on:click|preventDefault={() => {
-								openGithubPageRelease();
-							}}
-							target="_blank"
-							>New version v{latest} available!
-						</a>
-					{/if}
-				{/await}
-				<div>
-					<span>Source code:</span>
+						.reduce((acc, v) => acc + v, 0)}
 					<a
-						class="underline"
-						href="https://github.com/dwilkolek/wombat"
+						class="underline text-accent"
+						href="https://github.com/dwilkolek/wombat/releases/latest"
 						on:click|preventDefault={() => {
-							openGithubPage();
+							openGithubPageRelease();
 						}}
 						target="_blank"
-						>https://github.com/dwilkolek/wombat v{version}
+						>New version v{latest} available!
 					</a>
-				</div>
-				<div class="flex gap-1">
-					<span>User Id:</span>
-					<pre>{userId}</pre>
-				</div>
+				{/if}
+			{/await}
+			<div>
+				<span>Source code:</span>
+				<a
+					class="underline"
+					href="https://github.com/dwilkolek/wombat"
+					on:click|preventDefault={() => {
+						openGithubPage();
+					}}
+					target="_blank"
+					>https://github.com/dwilkolek/wombat v{version}
+				</a>
+			</div>
+			<div class="flex gap-1">
+				<span>User Id:</span>
+				<pre>{userId}</pre>
 			</div>
 		</div>
 	</div>
-{/await}
+</div>
