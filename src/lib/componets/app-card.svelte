649b3c35cf44663273d042131a1c2f6bce3c0c05
Damian Wilkolek <dwilkolek@protonmail.com>
2024-05-15 18:20:21 +0200

    feat: add infra checkmark per environment

--- src/lib/componets/app-card.svelte
+++ src/lib/componets/app-card.svelte
@@ -94,9 +94,25 @@
 				{#each $wombatProfileStore.environments as enabled_env}
 					{@const value = details.envs?.get(enabled_env)}
 					{#if displayConfig.envs == null || displayConfig.envs.includes(enabled_env)}
+						{@const hasInfraProfile = $wombatProfileStore.infraProfiles.some(
+							(infra) => infra.env == enabled_env && infra.app == app
+						)}
 						<div class={`flex flex-col app-env-cell px-2`}>
-							<div class="font-medium w-16 text-xs italic flex gap-1">
-								{enabled_env}:
+							<div class="font-medium text-xs italic flex gap-1 items-center">
+								<span>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 16 16"
+										fill="currentColor"
+										class={`w-3 h-3 ${hasInfraProfile ? 'text-amber-300' : 'opacity-60'}`}
+									>
+										<path
+											fill-rule="evenodd"
+											d="M15 8c0 .982-.472 1.854-1.202 2.402a2.995 2.995 0 0 1-.848 2.547 2.995 2.995 0 0 1-2.548.849A2.996 2.996 0 0 1 8 15a2.996 2.996 0 0 1-2.402-1.202 2.995 2.995 0 0 1-2.547-.848 2.995 2.995 0 0 1-.849-2.548A2.996 2.996 0 0 1 1 8c0-.982.472-1.854 1.202-2.402a2.995 2.995 0 0 1 .848-2.547 2.995 2.995 0 0 1 2.548-.849A2.995 2.995 0 0 1 8 1c.982 0 1.854.472 2.402 1.202a2.995 2.995 0 0 1 2.547.848c.695.695.978 1.645.849 2.548A2.996 2.996 0 0 1 15 8Zm-3.291-2.843a.75.75 0 0 1 .135 1.052l-4.25 5.5a.75.75 0 0 1-1.151.043l-2.25-2.5a.75.75 0 1 1 1.114-1.004l1.65 1.832 3.7-4.789a.75.75 0 0 1 1.052-.134Z"
+											clip-rule="evenodd"
+										/>
+									</svg></span
+								><span>{enabled_env}:</span>
 							</div>
 							<div class="flex gap-1 app-env-cell-stack">
 								{#if value}
