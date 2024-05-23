f2fcb8301e9eedbaa455804937f1c8b9a9f5049b
Damian Wilkolek <dwilkolek@protonmail.com>
2024-05-23 21:12:39 +0200

    mhh

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from './components/ui/button';
+import './style.css';
+
+function App() {
+	return (
+		<div>
+			<ul>
+				<ol>AWS CLI ✅</ol>
+				<ol>AWS session-manager plugin ✅</ol>
+				<ol>AWS session-manager plugin ✅</ol>
+			</ul>
+		</div>
+	);
+}
+
+export default App;
