d2170a846ac8c69dd00e6d69be82dbe7d874253c
Damian Wilkolek <dwilkolek@protonmail.com>
2024-07-02 22:16:39 +0200

    simpler extension

--- chrome-extension/aws-step-1.js
+++ chrome-extension/aws-step-1.js
@@ -2,6 +2,7 @@
 	const verifyBtn = document.querySelector('#cli_verification_btn');
 	if (verifyBtn) {
 		console.log('verify button found', verifyBtn);
+		window.umami.trackSafe('aws-step-1');
 		verifyBtn.click();
 	}
 })();
