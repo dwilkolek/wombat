c973e3a265ca29990ccc0dd1ee18c73adc259237
Damian Wilkolek <dwilkolek@protonmail.com>
2024-07-11 01:47:11 +0200

    fix: extension error handling

--- chrome-extension/snowflake.js
+++ chrome-extension/snowflake.js
@@ -1,14 +1,15 @@
-function execute(i) {
+async function execute(i) {
 	if (
 		document.body.innerHTML ==
 		'Your identity was confirmed and propagated to Snowflake JDBC driver. You can close this window now and go back where you started from.'
 	) {
-		window.umami.trackSafe('snowflake-auth');
+		await window.umami.trackSafe('snowflake-auth');
 		chrome.runtime.sendMessage({ action: 'closeTab' });
+		return;
 	}
 	if (i < 10) {
 		setTimeout(() => execute(i + 1), 200);
 	}
 }
 
-setTimeout(() => execute(0));
+setTimeout(async () => execute(0));
