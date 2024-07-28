1d3c54fc22b6a9ffed0aa0e87a1c0f443ee42e00
Damian Wilkolek <dwilkolek@protonmail.com>
2024-07-28 21:57:52 +0200

    Release v4.2.9

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,7 +1,7 @@
 /* global */
 /** @type {Array.<Cookie>} */
 let cookies = [];
-const extVersion = '4.2.8';
+const extVersion = '4.2.9';
 let syncEnabled = false;
 class Cookie {
 	constructor(name, value, env) {
