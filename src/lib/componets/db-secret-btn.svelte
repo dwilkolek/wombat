4b70fe0eda0e7ca7e2e987777e8852b00b35267c
Damian Wilkołek <dwilkolek@users.noreply.github.com>
2024-08-29 22:27:01 +0200

    FS rework (#145)
    * Make 4.2.22 required FS
    
    * Add FS refresh button
    
    * Tooltips and FS for all actions & prod env

--- src/lib/componets/db-secret-btn.svelte
+++ src/lib/componets/db-secret-btn.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import { writeText } from '@tauri-apps/api/clipboard';
-	import type { DatabaseCredentials, RdsInstance } from '$lib/types';
+	import { type DatabaseCredentials, type RdsInstance } from '$lib/types';
 	import { ask, message } from '@tauri-apps/api/dialog';
 	import { invoke } from '@tauri-apps/api/tauri';
-	import { wombatProfileStore } from '$lib/stores/available-profiles-store';
-	import { featuresStore } from '$lib/stores/feature-store';
 	import { userStore } from '$lib/stores/user-store';
+	import { getRdsSecretDisabledReason } from '$lib/stores/reasons';
+
 	export let database: RdsInstance | undefined;
 
+	$: disabledReason = getRdsSecretDisabledReason(database);
 	const credentialsHandler = async () => {
 		let answer = await ask(
 			'Are you alone and not sharing screen?\nAccess to credentials is recorded.\nRequires access to Secret Manager.',
@@ -39,43 +40,26 @@
 </script>
 
 {#if database}
-	{#if $featuresStore.devWay || $wombatProfileStore.infraProfiles.some(({ app, env }) => app == database?.normalized_name && env == database?.env)}
-		<div class="tooltip tooltip-left" data-tip="Search for secret">
-			<button
-				on:click={credentialsHandler}
-				data-umami-event="rds_credentials_get"
-				data-umami-event-uid={$userStore.id}
+	<div class="tooltip tooltip-left" data-tip={$disabledReason ?? 'Search for secret'}>
+		<button
+			disabled={!!$disabledReason}
+			class={$disabledReason ? 'opacity-30' : ''}
+			on:click={credentialsHandler}
+			data-umami-event="rds_credentials_get"
+			data-umami-event-uid={$userStore.id}
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 20 20"
+				fill="currentColor"
+				class="w-4 h-4 text-amber-300"
 			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-					class="w-4 h-4 text-amber-300"
-				>
-					<path
-						fill-rule="evenodd"
-						d="M8 7a5 5 0 113.61 4.804l-1.903 1.903A1 1 0 019 14H8v1a1 1 0 01-1 1H6v1a1 1 0 01-1 1H3a1 1 0 01-1-1v-2a1 1 0 01.293-.707L8.196 8.39A5.002 5.002 0 018 7zm5-3a.75.75 0 000 1.5A1.5 1.5 0 0114.5 7 .75.75 0 0016 7a3 3 0 00-3-3z"
-						clip-rule="evenodd"
-					/>
-				</svg>
-			</button>
-		</div>
-	{:else}
-		<div class="tooltip tooltip-left" data-tip={`Missing aws profile: ${database.normalized_name}`}>
-			<button class="opacity-30" disabled>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-					class="w-4 h-4 text-amber-300"
-				>
-					<path
-						fill-rule="evenodd"
-						d="M8 7a5 5 0 113.61 4.804l-1.903 1.903A1 1 0 019 14H8v1a1 1 0 01-1 1H6v1a1 1 0 01-1 1H3a1 1 0 01-1-1v-2a1 1 0 01.293-.707L8.196 8.39A5.002 5.002 0 018 7zm5-3a.75.75 0 000 1.5A1.5 1.5 0 0114.5 7 .75.75 0 0016 7a3 3 0 00-3-3z"
-						clip-rule="evenodd"
-					/>
-				</svg>
-			</button>
-		</div>
-	{/if}
+				<path
+					fill-rule="evenodd"
+					d="M8 7a5 5 0 113.61 4.804l-1.903 1.903A1 1 0 019 14H8v1a1 1 0 01-1 1H6v1a1 1 0 01-1 1H3a1 1 0 01-1-1v-2a1 1 0 01.293-.707L8.196 8.39A5.002 5.002 0 018 7zm5-3a.75.75 0 000 1.5A1.5 1.5 0 0114.5 7 .75.75 0 0016 7a3 3 0 00-3-3z"
+					clip-rule="evenodd"
+				/>
+			</svg>
+		</button>
+	</div>
 {/if}
