26049ecd6482422f7652b904494fad21c3e5840a
Damian Wilkołek <dwilkolek@users.noreply.github.com>
2024-09-09 19:12:02 +0200

    update chrome extension, readme and requirements (#152)
    * update chrome extension, readme and requirements
    
    * add link to chrome web store, publish extesion 5.0.1
    
    * fix typo in extension. version 5.0.2
    
    * fix link to chrome web store
    
    * fix link bump-version.js formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,14 +14,11 @@
 	import FeatureBtn from '$lib/componets/feature-btn.svelte';
 	import BrowserExtensionDot from '$lib/componets/browser-extension-dot.svelte';
 
+	const browserExtensionUrl =
+		'https://chromewebstore.google.com/detail/wombat-plugin/genpoikemhehdicnplfojdolhdhofonp';
+
 	const openGithubPage = () => {
 		open('https://github.com/dwilkolek/wombat');
-	};
-
-	const openExtInstallGuide = () => {
-		open(
-			'https://developer.chrome.com/docs/extensions/get-started/tutorial/hello-world#load-unpacked'
-		);
 	};
 
 	const { wombatAwsProfiles } = availableProfilesStore;
@@ -94,21 +91,17 @@
 			<ul class="list-disc ml-6">
 				<li>automates <span class="text-orange-300">aws sso login</span> process</li>
 				<li>automates github sign-in confirmation process</li>
-				<li>enables proxing to lambda services like commenting service</li>
+				<li>enables proxying to lambda services like commenting service</li>
 				<li>closes page after confirming identity by Snowflake JDBC driver</li>
 			</ul>
-
 			<a
 				on:click|preventDefault={() => {
-					openExtInstallGuide();
+					open(browserExtensionUrl);
 				}}
 				target="_blank"
-				href="https://developer.chrome.com/docs/extensions/get-started/tutorial/hello-world#load-unpacked"
-				>👉 <span class="underline text-amber-300 hover:text-amber-500">Installation guide</span></a
+				href={browserExtensionUrl}
+				>👉 <span class="underline text-amber-300 hover:text-amber-500">Chrome web store</span></a
 			>
-			{#await invoke('chrome_extension_dir') then dir}
-				<pre class="text-xs ml-6">{dir}</pre>
-			{/await}
 		</div>
 	</div>
 	<div class="hero-content flex-col">
@@ -194,12 +187,15 @@
 							<div class="text-rose-500">Required dependency is missing</div>
 						{/if}
 					{/await}
+
+					{#if $browserExtensionStatus.state === BrowserExtensionState.Outdated && ($browserExtensionStatus.version ?? '4.0').startsWith('4.')}
+						<p class="text-amber-500 mt-1">Please use extension from Chrome web store</p>
+					{/if}
 				</form>
 			</div>
 		</div>
 		<div class="flex flex-col justify-center items-center gap-2 my-2">
 			<UpdateBtn />
-
 			<div>
 				<span>Source code:</span>
 				<a
