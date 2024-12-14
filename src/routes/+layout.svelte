39935533a3f03833656f2ebb80ae00c522ba43b7
Damian Wilkołek <dwilkolek@users.noreply.github.com>
2024-12-14 16:46:45 +0100

    fix: remove context menu from release build (#206)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,9 @@
 	<ErrorBox />
 	{#await invoke('is_debug') then isDebug}
 		{#if !isDebug}
+			<script>
+				document.addEventListener('contextmenu', (event) => event.preventDefault());
+			</script>
 			<script
 				defer
 				src="https://umami.wilkolek.eu/script.js"
