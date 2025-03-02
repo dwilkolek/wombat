4d3711c0ec056f54ec33fe92e7692b397ba7a1ad
Damian Wilkolek <dwilkolek@protonmail.com>
2025-03-02 02:07:25 +0100

    nix ?

---
+++ shell.nix
@@ -0,0 +1,28 @@
+let
+  pkgs = import <nixpkgs> { };
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+    gobject-introspection
+    cargo
+    cargo-tauri
+    nodejs
+    nodePackages.pnpm
+  ];
+
+  buildInputs = with pkgs;[
+    at-spi2-atk
+    atkmm
+    cairo
+    gdk-pixbuf
+    glib
+    gtk3
+    harfbuzz
+    librsvg
+    libsoup_3
+    pango
+    webkitgtk_4_1
+    openssl
+  ];
+}
