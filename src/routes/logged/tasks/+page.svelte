83f17a7d2885a0c59f2992345562a35f6c5c122b
Damian Wilkołek <dwilkolek@users.noreply.github.com>
2025-03-12 23:34:07 +0100

    chore: ui tweaks (#215)

--- src/routes/logged/tasks/+page.svelte
+++ src/routes/logged/tasks/+page.svelte
@@ -7,29 +7,6 @@
 	<title>Tasks</title>
 	<meta name="description" content="Wombat" />
 </svelte:head>
-<div class="m-2 flex items-center">
-	<div role="alert" class="alert shadow-lg p-2">
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			class="stroke-warning shrink-0 h-6 w-6"
-			fill="none"
-			viewBox="0 0 24 24"
-			><path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-			/></svg
-		>
-		<div>
-			<h3 class="font-bold">This is BETA feature and requires installed chrome extension.</h3>
-			<div class="text-xs">
-				To have always valid session for specific environment you have to have open any app that do
-				some polling eg. for comments.
-			</div>
-		</div>
-	</div>
-</div>
 <div class="p-2 px-4">
 	<UserSessionProxyBtn />
 </div>
@@ -41,7 +18,7 @@
 				<tr>
 					<th>Arn</th>
 					<th>Port</th>
-					<th>Auth</th>
+					<th>Authentication interceptor</th>
 					<th>Status</th>
 				</tr>
 			</thead>
@@ -76,7 +53,6 @@
 						</td>
 					</tr>
 				{/each}
-				<!-- row 1 -->
 			</tbody>
 		</table>
 	</div>
