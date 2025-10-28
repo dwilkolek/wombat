c9da78ef326d5be67cf7aa2fe7729b4c6c48b0b2
Damian Wilkołek <dwilkolek@users.noreply.github.com>
2025-10-28 21:38:30 +0100

    feat: connection type select modal (#244)

--- src/lib/componets/db-task-status.svelte
+++ src/lib/componets/db-task-status.svelte
@@ -17,6 +17,7 @@
 			? $featuresStore.rdsConnWrite && $featuresStore.rdsProdConnWrite
 			: $featuresStore.rdsConnWrite
 	);
+	let dialog: HTMLDialogElement | undefined = $state();
 </script>
 
 {#if task && task.status !== TaskStatus.FAILED}
@@ -29,71 +30,31 @@
 					: 'Install dbeaver to get instant conneciton'}
 			>
 				<button
-					data-umami-event="dbeaver_open"
+					data-umami-event={rdsAllowWrites ? 'dbeaver_dialog' : 'dbeaver_open'}
 					data-umami-event-uid={$userStore.id}
 					disabled={!$userStore.dbeaver_path}
 					class={`link text-sm gap-1 text-amber-300 flex items-center ${
 						$userStore.dbeaver_path ? 'hover:text-amber-500 cursor-pointer' : 'hover:text-red-900'
 					}`}
 					onclick={() => {
-						execute(
-							'open_dbeaver',
-							{
-								db,
-								port: port,
-								readOnly: true
-							},
-							false
-						);
-					}}
-				>
-					{port}
-				</button>
-			</div>
-			{#if rdsAllowWrites}
-				<div
-					class="tooltip tooltip-left"
-					data-tip={$userStore.dbeaver_path
-						? 'Open write connection in dbeaver'
-						: 'Install dbeaver to get instant conneciton'}
-				>
-					<!-- svelte-ignore a11y_consider_explicit_label -->
-					<button
-						data-umami-event="dbeaver_open_write"
-						data-umami-event-uid={$userStore.id}
-						disabled={!$userStore.dbeaver_path}
-						class={`link text-sm gap-1 text-amber-300 flex items-center ${
-							$userStore.dbeaver_path ? 'hover:text-amber-500 cursor-pointer' : 'hover:text-red-900'
-						}`}
-						onclick={() => {
+						if (rdsAllowWrites) {
+							dialog?.show();
+						} else {
 							execute(
 								'open_dbeaver',
 								{
 									db,
 									port: port,
-									readOnly: false
+									readOnly: true
 								},
 								false
 							);
-						}}
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="size-5"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-							/>
-						</svg>
-					</button>
-				</div>
-			{/if}
+						}
+					}}
+				>
+					{port}
+				</button>
+			</div>
 		</div>
 	{:else}
 		<span class="text-sm text-amber-300/[.6] animate-pulse">{port}</span>
@@ -101,3 +62,58 @@
 {:else}
 	<span class="text-sm text-gray-600">{port}</span>
 {/if}
+
+<dialog bind:this={dialog} onclose={() => console.log('closed')} class="modal">
+	<div class="modal-box">
+		<h3 class="text-lg font-bold">Open Dbeaver console</h3>
+		<p class="py-4">
+			All connections are opened with <span class="font-mono text-warning">autoCommit=false</span>.
+			<br />
+			Read only mode depends on DBeaver version and JDBC driver
+			<span class="font-mono text-warning">readOnly</span> property.
+		</p>
+		<div class="modal-action">
+			<button
+				class="btn btn-info"
+				data-umami-event="dbeaver_open_read"
+				data-umami-event-uid={$userStore.id}
+				disabled={!$userStore.dbeaver_path}
+				onclick={() => {
+					execute(
+						'open_dbeaver',
+						{
+							db,
+							port: port,
+							readOnly: true
+						},
+						false
+					);
+
+					dialog?.close();
+				}}
+			>
+				Read only mode
+			</button>
+			<button
+				class="btn btn-warning"
+				data-umami-event="dbeaver_open_write"
+				data-umami-event-uid={$userStore.id}
+				disabled={!$userStore.dbeaver_path}
+				onclick={() => {
+					execute(
+						'open_dbeaver',
+						{
+							db,
+							port: port,
+							readOnly: false
+						},
+						false
+					);
+					dialog?.close();
+				}}
+			>
+				Write mode
+			</button>
+		</div>
+	</div>
+</dialog>
