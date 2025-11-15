ba4204dae948efaf21c9aef10b6b8ae896d6ecb8
Damian Wilkolek <dwilkolek@protonmail.com>
2025-11-15 03:24:29 +0100

    chore: requirements for v5

--- src-tauri/src/requirements.rs
+++ src-tauri/src/requirements.rs
@@ -1,3 +1,3 @@
-pub static REQUIRED_FEATURE: &str = "wombat-4.12.0";
-pub static EXPECTED_BROWSER_EXTENSION: &str = "5.0.0";
-pub static LAST_SUPPORTED_BROWSER_EXTENSION: &str = "5.0.0";
+pub static REQUIRED_FEATURE: &str = "wombat-5.0.0";
+pub static EXPECTED_BROWSER_EXTENSION: &str = "6.0.3";
+pub static LAST_SUPPORTED_BROWSER_EXTENSION: &str = "6.0.3";
